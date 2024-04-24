7e42076bfdcbe6ae095c6b3a6a552fe278b3e35f
npardheji <121021537+npardheji@users.noreply.github.com>
2024-04-24 16:26:07 +0530

    Update settings

--- Downloader/settings.py
+++ Downloader/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-2kcx(+7p%lfa)sf8muyurgavwdjw*l+46y&@)ph9uiymouhur#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
