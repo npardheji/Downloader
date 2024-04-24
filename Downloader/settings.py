2946483f70f715fcb1ab01ae3e0b7fc3731bfdcc
npardheji <121021537+npardheji@users.noreply.github.com>
2024-04-24 15:37:52 +0530

    sec add

--- Downloader/settings.py
+++ Downloader/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -84,12 +85,8 @@ WSGI_APPLICATION = 'Downloader.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'ytdb',
-        'USER': 'root',
-        'PASSWORD': 'Nitesh@13',
-        'HOST':'localhost',
-        'PORT':'3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
