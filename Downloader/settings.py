b5a11beb7df439c74e038c953eb161cc1a3d6a8b
npardheji <121021537+npardheji@users.noreply.github.com>
2024-04-24 16:01:46 +0530

    for versel

--- Downloader/settings.py
+++ Downloader/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-2kcx(+7p%lfa)sf8muyurgavwdjw*l+46y&@)ph9uiymouhur#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -132,3 +132,8 @@ MEDIA_ROOT=BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build/', 'static')
